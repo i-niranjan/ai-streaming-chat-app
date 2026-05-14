55df458884cc486a46915b3f6dba937562518f54
i-niranjan <niranjanchaudhari2004@gmail.com>
2026-05-14 06:11:32 +0100

    adding req. stack

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -3,6 +3,8 @@ import { streamText, type UIMessage, convertToModelMessages } from "ai";
 export async function POST(req: Request) {
   const { messages }: { messages: UIMessage[] } = await req.json();
 
+  console.log(JSON.stringify(messages));
+
   const result = streamText({
     model: "openai/gpt-5-mini",
     messages: await convertToModelMessages(messages),
---
+++ docs/TODO.md
@@ -0,0 +1,5 @@
+- Need to develop a decent prompt viewer, to view the text from ai in particular format
+
+- Develop a initial schema
+
+-
